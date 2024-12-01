f7a55bdb9a09ebc8b92dd678b2ec8aa9d7c97380
AspectOfTheNoob <Mcthouacbb@gmail.com>
2024-12-01 14:47:40 -0500

    Basic input handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{io, str::SplitWhitespace};
+
+fn solve_command(tokens: SplitWhitespace<'_>) {
+    todo!();
+}
+
+fn generate_command(tokens: SplitWhitespace<'_>) {
+    todo!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut cmd = String::new();
+        io::stdin()
+            .read_line(&mut cmd)
+            .expect("Failed to read line");
+
+        let mut tokens = cmd.trim().split_whitespace();
+        match tokens.next() {
+            Some("solve") => {
+                solve_command(tokens);
+            }
+            Some("generate") => {
+                generate_command(tokens);
+            }
+            _ => {
+                print!("Unknown command: {}", cmd);
+            }
+        }
+    }
 }
